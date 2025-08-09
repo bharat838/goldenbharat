a0755b195c6d1e90acb75c48ca7a30aa1ca2f0be
Bharat Khandelwal <91523611+bharat838@users.noreply.github.com>
2025-08-10 02:17:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(cors({
 }));
 
 
-app.use(cors());
-//app.use(express.json());
+//app.use(cors());
+app.use(express.json());
 
 // ✅ Create HTTP server (no Socket.IO now)
 const server = http.createServer(app);
@@ -116,6 +116,7 @@ server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 // // ✅ Start server
 // const PORT = process.env.PORT || 5000;
 // server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 
