4afe2d87f26f6c35240ec0e6a6c65b5def2eeb95
Bharat Khandelwal <91523611+bharat838@users.noreply.github.com>
2025-08-10 01:57:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,9 @@ const app = express();
 
 // ✅ Updated CORS Configuration
 app.use(cors({
-  origin: ["https://goldenbharat.netlify.app", "http://localhost:5173"], // React + Vite ports
+  // origin: ["https://goldenbharat.netlify.app", "http://localhost:5173"], // React + Vite ports
+  origin: 'https://goldenbharat.netlify.app',
+  optionsSuccessStatus: 200,
   credentials: true
 }));
 app.use(cors(corsOptions));
@@ -114,6 +116,7 @@ server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 // // ✅ Start server
 // const PORT = process.env.PORT || 5000;
 // server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 
