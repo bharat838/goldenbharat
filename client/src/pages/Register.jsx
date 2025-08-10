6570f9b1ebfa9387e1cb1ba310590ae463596216
Bharat Khandelwal <91523611+bharat838@users.noreply.github.com>
2025-08-10 13:31:54 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://goldenbharat.netlify.app/api/auth/register", form);
+      const res = await axios.post("http://localhost:5000/api/auth/register", form);
       alert(res.data.message || "Registration successful! Please login.");
       navigate("/login");
     } catch (err) {
@@ -219,4 +219,5 @@ export default Register;
 
 
 //----- for testing -------
+
 
